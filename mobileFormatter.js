d9d07f4be71b8d62dab25829c0595cab59a6fae0
Patrick Liem <patrickliem00@gmail.com>
2020-02-12 15:43:02 -0500

    Added main functionality
    Basically added all of the resizing stuff the client initially asked for

--- mobileFormatter.js
+++ mobileFormatter.js
@@ -7,7 +7,28 @@
 function conditionallyConvertToMobile(htmlString) {
     // If the window size is mobile-sized. 1024 is the width of the Amazon Kindle Fire. Subject to change.
     if ($( window ).width() <= 1024) {
-        return htmlString;
+
+        // Create a dummy DOM element and add our html to it
+        let dummy = $("<div></div>");
+        $(dummy).append(htmlString);
+
+        // The contact information div is the third div
+        let col2 = $(dummy).find("div")[3];
+        console.log(col2);
+
+        // The place to put the contact information div is the first (and only) br
+        let heading = $(dummy).find("br")[0];
+        console.log(heading);
+
+        // Detach the contact information column and adjust the CSS to make it not float
+        let contactInfo = $(col2).detach();
+        $(contactInfo).css("float", "none");
+        $(contactInfo).css("position", "static");
+
+        // Add the contact information div back in
+        $(heading).after(contactInfo);
+
+        return $(dummy).html();
     }
 
     return htmlString;
